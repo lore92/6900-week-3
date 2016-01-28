b43b74a94dba3a94d4a029f308ec38527eaa65d3
siqister <siqi.zhu@gmail.com>
2016-01-27 22:55:01 -0500

    First commit

---
+++ task-1/script.js
@@ -0,0 +1,34 @@
+var m = {t:50,r:100,b:50,l:100},
+    w = d3.select('.plot').node().clientWidth- m.l- m.r,
+    h = d3.select('.plot').node().clientHeight - m.t - m.b;
+
+var plot = d3.select('.plot').append('svg')
+    .attr('width',w+ m.l+ m.r)
+    .attr('height',h+ m.t+ m.b)
+    .append('g').attr('class','histogram')
+    .attr('transform','translate('+ m.l+','+ m.t+')');
+
+d3.csv('../data/hubway_trips.csv',parse,dataLoaded);
+
+function dataLoaded(err,rows){
+}
+
+function parse(d){
+    if(+d.duration<0) return;
+
+    return {
+        duration: +d.duration,
+        startTime: parseDate(d.start_date),
+        endTime: parseDate(d.end_date),
+        startStation: d.strt_statn,
+        endStation: d.end_statn
+    }
+}
+
+function parseDate(date){
+    var day = date.split(' ')[0].split('/'),
+        time = date.split(' ')[1].split(':');
+
+    return new Date(+day[2],+day[0]-1, +day[1], +time[0], +time[1]);
+}
+
---
+++ task-2/script.js
@@ -0,0 +1,5 @@
+var width = d3.select('.plot').node().clientWidth,
+    height = d3.select('.plot').node().clientHeight;
+
+
+var values = d3.range(20).map(function(){return Math.random();});
